700062375e00d665d2d076e3fa19f9829c7c8d6a
futurebeets <futurebeets1@gmail.com>
2024-04-07 01:55:00 -0400

    feature added

--- script.js
+++ script.js
@@ -1,103 +1,105 @@
 let cards = document.querySelector(".cards");
+let getApi = 'https://script.google.com/macros/s/AKfycbyc72F11GudEPHXTIDOazinHS6-uhn6YnBEsOK3ei_WwsgtaiQkMo6SB8pdDb1rsRM/exec';
+let data
 
-let data = [
+data = [
   {
     image: "./images/egg-puffs.jpg",
     name: "Egg Puffs",
     para: "Egg Puffs In Air Feyer Food in kerala",
     category: "Snacks",
-    price: "12.00",
+    price: "Load..",
   },
   {
     image: "./images/nutella-banana-puffs.jpg",
     name: "Banana Puffs",
     para: "Banana Puffs In Air Feyer Food in kerala",
     category: "Snacks",
-    price: "10.00",
+    price: "Load..",
   },
   {
     image: "./images/maxresdefault.jpg",
     name: "Chiken Puffs",
     para: "Chiken Puffs In Air Feyer Food in kerala",
     category: "Snacks",
-    price: "15.00",
+    price: "Load..",
   },
   {
     image: "./images/Veg-Puff-500x375.jpg",
     name: "Vegitable Puffs",
     para: "Vegitable Puffs In Air Feyer Food in kerala",
     category: "Snacks",
-    price: "13.00",
+    price: "Load..",
   },
   {
     image: "./images/Crispy-Chicken-Burger-square-FS-4518.jpg",
     name: "Chicken Burger",
     para: "Chicken Burger Cheesi Food in kerala",
     category: "Snacks",
-    price: "20.00",
+    price: "Load..",
   },
   {
     image: "./images/lime-juice.jpg",
     name: "Lime Juic",
     para: "Special Lime,Apple mixed Juic in kerala",
     category: "Drinks",
-    price: "15.00",
+    price: "Load..",
   },
   {
     image: "./images/Masala-Chai-Tea-Recipe-Card.jpg",
     name: "Chai & Coffee",
     para: "A milky and spiced tea, perfect for a morning ",
     category: "Drinks",
-    price: "10.00",
+    price: "Load..",
   },
   {
     image: "./images/Sharjah-Shake-4.jpg",
     name: "Sharjah Shake",
     para: "A sweet and creamy milkshake-like drink traditionally",
     category: "Drinks",
-    price: "25.00",
+    price: "Load..",
   },
   {
     image: "./images/Rabri-Falooda-1300x867.jpg",
     name: "Falooda",
     para: "Traditionally it is made by mixing ice cream,milk etc.",
     category: "Drinks",
-    price: "30.00",
+    price: "Load..",
   },
   {
     image: "./images/juices-shutterstock_121270552.jpg",
     name: "Juices",
     para: "Apple,Orange,Mango,Banana,watermelon,greap etc.",
     category: "Drinks",
-    price: "14.00",
+    price: "Load..",
   },
   {
     image: "./images/53099699.cms.jpg",
     name: "Jilebi",
     para: " fried, crispy and sugar-coated.Best Desert in India",
     category: "Deserts",
-    price: "10.00",
+    price: "Load..",
   },
   {
     image: "./images/niceLadoo.jpg",
     name: "Laddu",
     para: " Ladoo, also known as laddu, are soft dessert balls",
     category: "Deserts",
-    price: "12.00",
+    price: "Load..",
   },
   {
     image: "./images/MysorePak_01.jpg",
     name: "Mysore Pak",
     para: " Melt-in-your-mouth Mysore pak is often made one of two ways",
     category: "Deserts",
-    price: "15.00",
+    price: "Load..",
   },
   {
     image: "./images/6541218173_5a172ebe8c_z1.jpg",
     name: "Plum Cake",
     para: " Sweet and tasty plum cake air cooked in India",
     category: "Deserts",
-    price: "10.00",
+    price: "Load..",
   },
   {
     image:
@@ -105,52 +107,58 @@ let data = [
     name: "Cream Cake",
     para: " Sprecial desert butter cream cotted cake.sweety and tasty",
     category: "Deserts",
-    price: "10.00",
+    price: "Load..",
   },
   {
     image: "./images/noodles.jpg",
     name: "Noodles",
     para: " Sprecial Maggi noodles for eat two person",
     category: "Special",
-    price: "15.00",
+    price: "Load..",
   },
   {
     image: "./images/egg-puffs.jpg",
     name: "Cream Cake",
     para: " Sprecial desert butter cream cotted cake.sweety and tasty",
     category: "Special",
-    price: "10.00",
+    price: "Load..",
   },
   {
     image: "./images/lime-juice.jpg",
     name: "Lime Juic",
     para: "Special Lime,Apple mixed Juic in kerala",
     category: "Special",
-    price: "15.00",
+    price: "Load..",
   },
   {
     image: "./images/Masala-Chai-Tea-Recipe-Card.jpg",
     name: "Chai & Coffee",
     para: "A milky and spiced tea, perfect for a morning ",
     category: "Special",
-    price: "10.00",
+    price: "Load..",
   },
   {
     image: "./images/egg-puffs.jpg",
     name: "Egg Puffs",
     para: "Egg Puffs In Air Feyer Food in kerala",
     category: "Special",
-    price: "12.00",
+    price: "Load..",
   },
   {
     image: "./images/Crispy-Chicken-Burger-square-FS-4518.jpg",
     name: "Chicken Burger",
     para: "Chicken Burger Cheesi Food in kerala",
     category: "Special",
-    price: "20.00",
+    price: "Load..",
+  },
+  {
+    image: "./images/biriyani.jpg",
+    name: "Chicken Biriyani",
+    para: "Spicy Special Chiken Biriyani in kerala",
+    category: "Special",
+    price: "Load..",
   },
 ];
-
 function category(name,cname) {
     cards.innerHTML = '';
 
@@ -215,3 +223,17 @@ function category(name,cname) {
 }
 
 category("");
+function getData(){
+  fetch(getApi).then(response => response.json())
+  .then(res => {
+    console.log(res);
+data = res;
+category('')
+  }).catch((err)=>{
+    window.location.reload()
+  })
+}
+
+getData()
+
+console.log(data);
